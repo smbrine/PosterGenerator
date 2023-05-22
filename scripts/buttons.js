7b3f7baf449e4c9d40da48cf674761e5c219ad3a
smbrine <smbrine@yandex.ru>
2023-05-22 14:01:17 +0300

    update 7 files and delete 41 files

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -9,6 +9,13 @@ xhr.open("GET", "./resources/outcomes.json", true);
 // Set the responseType to "json"
 xhr.responseType = "json";
 
+function createButton(label, clickHandler) {
+  var button = document.createElement("button");
+  button.textContent = label;
+  button.addEventListener("click", clickHandler);
+  return button;
+}
+
 // Add an event listener to handle the onload event
 xhr.onload = function() {
   // Check if the status is OK
@@ -19,42 +26,84 @@ xhr.onload = function() {
     // Loop through each key in the JSON data object and create a button element for each key
     for (var label in outcomesDataset) {
       // Create a new button element
-      var button = document.createElement("button");
-      
-      // Set the label of the button to the current key
-      button.textContent = label;
-      button.classList = "button-created"
-      
-      // Add an event listener to the button that records its label when clicked
-      button.addEventListener("click", function(event) {
+      var button = createButton(label, function(event) {
         // Remove all previously created buttons
         buttonContainer.innerHTML = '';
-      
+
         // Get the label of the clicked button
         var clickedLabel = event.target.textContent;
-      
+
         // Loop through each key in the next level of nested data and create a button element for each key
         for (var label2 in outcomesDataset[clickedLabel]) {
           // Create a new button element
-          var button2 = document.createElement("button");
-      
-          // Set the label of the button to the current key
-          button2.textContent = label2;
-      
-          // Add an event listener to the button that records its label when clicked
-          button2.addEventListener("click", function(event) {
+          var button2 = createButton(label2, function(event) {
+            // Remove all previously created buttons
+            buttonContainer.innerHTML = '';
+
             // Get the label of the clicked button
             var clickedLabel2 = event.target.textContent;
-      
-            // Do something with the clicked label
-            console.log(clickedLabel2);
+
+            // Loop through each key in the next level of nested data (Key3)
+            for (var label3 in outcomesDataset[clickedLabel][clickedLabel2]) {
+              var button3 = createButton(label3, function(event) {
+                // Remove all other button3 elements
+                buttonContainer.innerHTML = '';
+
+                // Get the label of the clicked button
+                var clickedLabel3 = event.target.textContent;
+
+                // Loop through each key in the next level of nested data (Key4)
+                for (var label4 in outcomesDataset[clickedLabel][clickedLabel2][clickedLabel3]) {
+                  var button4 = createButton(label4, function(event) {
+                    // Remove all other button4 elements
+                    buttonContainer.innerHTML = '';
+
+                    // Get the label of the clicked button
+                    var clickedLabel4 = event.target.textContent;
+
+                    // Check if the current key is "Pic"
+                      // Get the path to the picture
+                      buttonContainer.innerHTML = '';
+                      var picPath = outcomesDataset[clickedLabel][clickedLabel2][clickedLabel3][clickedLabel4]['Pic'];
+                      console.log(picPath)
+
+                      // Step 1: Create the element
+                      var posterIMG = document.createElement('img');
+
+                      // Step 2: Set attributes and content
+                      // newElement.id = 'poster-image';
+                      posterIMG.src = picPath
+
+                      // Step 3: Select the parent element
+                      var parentElment = document.getElementById('poster-img');
+
+                      // Step 4: Add the new element to the parent element
+                      parentElment.appendChild(posterIMG);
+
+                    
+                  });
+
+                  // Add a class to button4 for easier removal
+                  button4.classList.add("button4");
+
+                  // Add the button to the parent element
+                  buttonContainer.appendChild(button4);
+                }
+              });
+
+              // Add a class to button3 for easier removal
+              button3.classList.add("button3");
+
+              // Add the button to the parent element
+              buttonContainer.appendChild(button3);
+            }
           });
-      
+
           // Add the button to the parent element
           buttonContainer.appendChild(button2);
         }
       });
-      
+
       // Add the button to the parent element
       buttonContainer.appendChild(button);
     }
